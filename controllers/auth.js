33d27a8c7c59ce476e5496d4da5c7fa2821674ca
rohanpawar0203 <103638389+rohanpawar0203@users.noreply.github.com>
2023-07-10 10:11:18 +0530

    User registration changed

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,26 @@ import User from "../models/User.js";
 // /* REGISTER USER */
 export const register = async (req, res) => {
   try {
-    const { firstName, lastName, email, password, contact } = req.body;
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      contact,
+      type,
+      role,
+      companyName,
+      revenuePerYear,
+      gst_in,
+      pan_number,
+      aadhar_number,
+      account_number,
+      account_name,
+      ifsc_code,
+      profile_status,
+      address,
+      pincode,
+    } = req.body;
 
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
@@ -14,8 +33,21 @@ export const register = async (req, res) => {
       firstName,
       lastName,
       email,
-      password: passwordHash,
+      // password: passwordHash,
       contact,
+      type,
+      role,
+      companyName,
+      revenuePerYear,
+      gst_in,
+      pan_number,
+      aadhar_number,
+      account_number,
+      account_name,
+      ifsc_code,
+      profile_status,
+      address,
+      pincode,
     });
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
--- models/User.js
+++ models/User.js
@@ -4,31 +4,66 @@ const UserSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
-      required: true,
+      // required: true,
       min: 2,
       max: 50,
     },
     lastName: {
       type: String,
-      required: true,
+      // required: true,
       min: 2,
       max: 50,
     },
     email: {
       type: String,
-      required: true,
+      // required: true,
       max: 50,
       unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      min: 5,
     },
     contact: {
       type: Number,
       required: true,
       min: 10,
+    },
+    address: {
+      type: String,
+      max: 100,
+    },
+    pincode: {
+      type: String,
+    },
+    type: {
+      type: String,
+    },
+    role: {
+      type: String,
+    },
+    companyName: {
+      type: String,
+    },
+    revenuePerYear: {
+      type: String,
+    },
+    gst_in: {
+      type: String,
+    },
+    pan_number: {
+      type: String,
+    },
+    aadhar_number: {
+      type: String,
+    },
+    account_number: {
+      type: String,
+    },
+    account_name: {
+      type: String,
+    },
+    ifsc_code: {
+      type: String,
+    },
+    profile_status: {
+      type: String,
     },
   },
   { timestamps: true }
